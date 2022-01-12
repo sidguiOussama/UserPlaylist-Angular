2d0948a2b7ac5f81ce43349954c8d174bfffa596
sidguiOussama <=>
2022-01-12 12:23:18 +0100

    prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,9 @@
 export const environment = {
   production: true,
+  authUrl: 'https://user-playlist-api.herokuapp.com/api/v1/auth/',
+  userUrl: 'https://user-playlist-api.herokuapp.com/api/v1/Users/',
+  playlistUrl: 'https://user-playlist-api.herokuapp.com/api/v1/Playlists/',
+  advertUrl: 'https://user-playlist-api.herokuapp.com/api/v1/Ads/',
   firebaseConfig : {
     apiKey: "AIzaSyAJLkOAuf4L_KSTcsdCYoXHEnmbhRs4E9E",
     authDomain: "adsimages-9bd1a.firebaseapp.com",
